fdd1289a309f1bf990e2363b605edf3b7b7df6c1
advanced-go <mark.wilson.go@outlook.com>
2024-11-18 12:50:04 -0600

    Updating

--- go.mod
+++ go.mod
@@ -2,7 +2,7 @@ module github.com/advanced-go/resiliency
 
 go 1.21.4
 
-require github.com/advanced-go/common v0.0.0-20241107192729-529f35719a51
+require github.com/advanced-go/common v0.0.0-20241118183807-a68e028bdf96
 
 require (
 	github.com/advanced-go/log v0.0.0-20241114205934-ffe4b82174dd // indirect
--- guidance/guidance.go
+++ guidance/guidance.go
@@ -2,24 +2,25 @@ package guidance
 
 import (
 	"github.com/advanced-go/common/core"
+	"github.com/advanced-go/common/messaging"
 )
 
 // Guidance - guidance functions struct, a nod to Linus Torvalds and plain C
 type Guidance struct {
-	Assignments    func(h core.ErrorHandler, origin core.Origin) ([]HostEntry, *core.Status)
-	NewAssignments func(h core.ErrorHandler, origin core.Origin) ([]HostEntry, *core.Status)
+	Assignments    func(h messaging.Error, origin core.Origin) ([]HostEntry, *core.Status)
+	NewAssignments func(h messaging.Error, origin core.Origin) ([]HostEntry, *core.Status)
 }
 
 var Guide = func() *Guidance {
 	return &Guidance{
-		Assignments: func(h core.ErrorHandler, origin core.Origin) ([]HostEntry, *core.Status) {
+		Assignments: func(h messaging.Error, origin core.Origin) ([]HostEntry, *core.Status) {
 			e, status := GetEntry(origin)
 			if !status.OK() {
-				h.Handle(status)
+				h.Exception(status)
 			}
 			return []HostEntry{e}, status
 		},
-		NewAssignments: func(h core.ErrorHandler, origin core.Origin) ([]HostEntry, *core.Status) {
+		NewAssignments: func(h messaging.Error, origin core.Origin) ([]HostEntry, *core.Status) {
 			return nil, core.StatusNotFound()
 		},
 	}
