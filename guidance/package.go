2f24a70264af0cf1906e10f482a6b6074dd2bf24
advanced-go <mark.wilson.go@outlook.com>
2024-11-23 07:53:40 -0600

    Update package.go

--- guidance/package.go
+++ guidance/package.go
@@ -5,16 +5,16 @@ import "github.com/advanced-go/common/core"
 const (
 	PkgPath    = "github/advanced-go/resiliency/guidance"
 	WestRegion = "us-west1"
-	WestZoneA  = "us-west1-a"
-	WestZoneB  = "us-west1-b"
+	WestZoneA  = "w-a"
+	WestZoneB  = "w-b"
 
 	CentralRegion = "us-central1"
-	CentralZoneA  = "us-central1-a"
-	CentralZoneB  = "us-central1-b"
+	CentralZoneA  = "c-a"
+	CentralZoneB  = "c-b"
 
 	EastRegion = "us-east1"
-	EastZoneA  = "us-east1-a"
-	EastZoneB  = "us-east1-b"
+	EastZoneA  = "e-a"
+	EastZoneB  = "e-b"
 )
 
 func GetRegion(origin core.Origin) ([]HostEntry, *core.Status) {
